7ed4e5ef2f4e8ed00721673ba60fc6649acc4d77
Bahoz <julienbraultcherif@gmail.com>
2025-08-01 20:15:21 +0200

    add logo 192x192

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,11 @@ export const metadata: Metadata = {
   title: "Todolist Next.js x Express.js",
   description: "A todolist created with Express.js",
   icons: {
-    icon: [{ url: "/assets/favicon.png", sizes: "32x32", type: "image/x-icon" },]
-  }
+    icon: [
+      { url: "/assets/favicon.png", sizes: "32x32", type: "image/x-icon" },
+      { url: "/assets/logo_192x192.png", sizes: "192x192", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({
